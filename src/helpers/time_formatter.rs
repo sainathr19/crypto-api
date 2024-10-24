d60cce1c7d5c654ff985fbcd0e6a438894b4bfae
sainath <88471927+sainathr19@users.noreply.github.com>
2024-10-24 22:24:05 +0530

    removed deprecated usages

--- src/helpers/time_formatter.rs
+++ src/helpers/time_formatter.rs
@@ -3,7 +3,7 @@ use chrono::NaiveDateTime;
 
 pub fn parse_date(date_str: &str) -> Result<i64, HttpResponse> {
     match NaiveDateTime::parse_from_str(date_str, "%Y-%m-%dT%H:%M:%S") {
-        Ok(datetime) => Ok(datetime.timestamp()),
+        Ok(datetime) => Ok(datetime.and_utc().timestamp()),
         Err(_) => {
             Err(HttpResponse::BadRequest().body("Invalid date format. Use 'YYYY-MM-DDTHH:MM:SS'."))
         }
